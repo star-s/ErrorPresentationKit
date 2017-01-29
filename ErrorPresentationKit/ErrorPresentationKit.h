965fa420d08a8767d4f9ad108f129b4eec893c7e
Sergey Starukhin <star.s@me.com>
2017-01-29 17:58:06 +0400

    Add framework project

---
+++ ErrorPresentationKit/ErrorPresentationKit.h
@@ -0,0 +1,19 @@
+//
+//  ErrorPresentationKit.h
+//  ErrorPresentationKit
+//
+//  Created by Sergey Starukhin on 29.01.17.
+//  Copyright © 2017 Sergey Starukhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ErrorPresentationKit.
+FOUNDATION_EXPORT double ErrorPresentationKitVersionNumber;
+
+//! Project version string for ErrorPresentationKit.
+FOUNDATION_EXPORT const unsigned char ErrorPresentationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ErrorPresentationKit/PublicHeader.h>
+
+
